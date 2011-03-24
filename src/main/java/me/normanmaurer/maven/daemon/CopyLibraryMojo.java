8591011485a8d0e239bde4ecb71f04cb3545c530
Norman Maurer <norman@apache.org>
2011-03-24 10:14:26 +0100

    rename package to me.normanmaurer

--- src/main/java/me/normanmaurer/maven/daemon/CopyLibraryMojo.java
+++ src/main/java/me/normanmaurer/maven/daemon/CopyLibraryMojo.java
@@ -1,4 +1,4 @@
-package me.maurer.norman.maven.daemon;
+package me.normanmaurer.maven.daemon;
 /****************************************************************
  * Licensed to the Apache Software Foundation (ASF) under one   *
  * or more contributor license agreements.  See the NOTICE file *
